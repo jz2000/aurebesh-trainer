177450e913beffdfae217ca0ee472994004a279f
sergei.zhezniakowski <sergei.zhezniakowski@nytimes.com>
2023-12-31 14:42:31 -0500

    migration to react components: generic types

---
+++ src/components/abtTypes.ts
@@ -0,0 +1,9 @@
+export type AbtSession = {
+    tasks : number,
+    hits : number,
+    misses : number,
+    lastHit: Date,
+    lastMiss: Date,
+};
+
+export type AbtUiLanguage = 'aurebesh' | 'high_galactic';
