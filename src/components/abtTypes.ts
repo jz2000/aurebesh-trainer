2eb632a64a8954e0251a4c1a7f0454f6242a6e5b
sergei.zhezniakowski <sergei.zhezniakowski@nytimes.com>
2023-12-31 18:00:55 -0500

    migration to react components: types refinement to support last answer

--- src/components/abtTypes.ts
+++ src/components/abtTypes.ts
@@ -4,16 +4,20 @@ export type AbtTask = {
     hits: number;
     misses: number;
     hitsAfterLastMiss: number;
+    lastAnswer: string;
+    lastAnswerStatus: AbtTaskAnswerStatus;
 };
 
 export type AbtTaskAnswerStatus = 'correct' | 'incorrect' | 'idle';
 
 export type AbtSession = {
-    tasks : number,
-    hits : number,
-    misses : number,
+    totalAttempts : number,
+    totalHits : number,
+    totalMisses : number,
     lastHit: Date,
     lastMiss: Date,
+    currentTask: AbtTask;
+    lastTask: AbtTask | null;
 };
 
 export type AbtUiLanguage = 'aurebesh' | 'high_galactic';
