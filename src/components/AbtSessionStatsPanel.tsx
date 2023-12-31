e3cd0448f491c7554cfb499bd603f101de448314
sergei.zhezniakowski <sergei.zhezniakowski@nytimes.com>
2023-12-31 14:54:07 -0500

    migration to react components: session stats panel component

---
+++ src/components/AbtSessionStatsPanel.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styles from "*.module.css";
+
+export type AbtSessionStatsPanelProps = {
+
+};
+
+export const AbtSessionStatsPanel = (props: AbtSessionStatsPanelProps): React.ReactElement => {
+    return (
+        <div className={styles['b-statistics']}>
+            <div className={styles['b-statistics__caption']}>
+                <span>Result</span>
+            </div>
+            <div className={styles['b-statistics__container']}>
+                <div className={styles['b-statistics__answer']}>
+                    <div className={styles['b-answer-indicator']}>
+                        <div className={styles['b-answer-indicator__text']} id="js-answer-text">Idle</div>
+                        <div className={styles['b-answer-indicator__explanation']} id="js-answer-explanation">
+                            <span className={styles['b-answer-indicator__explanation__aub']} id="js-answer-explanation__aub">A</span>
+                            =
+                            <span className={styles['b-answer-indicator__explanation__hg']} id="js-answer-explanation__hg">?</span>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles['b-statistics__totals']}>
+                    <div className={styles['b-statistics__row']}>
+                        <span className={styles['b-statistics__row-name']}>Attempts </span>
+                        <span className={styles['b-statistics__row-value']} id="js-total-tasks">0</span>
+                    </div>
+                    <div className={styles['b-statistics__row']}>
+                        <span className={styles['b-statistics__row-name']}>Hits </span>
+                        <span className={styles['b-statistics__row-value']} id="js-total-hits">0</span>
+                    </div>
+                    <div className={styles['b-statistics__row']}>
+                        <span className={styles['b-statistics__row-name']}>Misses </span>
+                        <span className={styles['b-statistics__row-value']} id="js-total-misses">0</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import styles from '@/styles/Home.module.css'
 import { AbtUiLanguageControl } from '../components/AbtUiLanguageControl';
 import { AbtUiLanguage } from '../components/abtTypes';
 import { AbtActiveTaskPanel } from '../components/AbtActiveTaskPanel';
+import { AbtSessionStatsPanel } from '../components/AbtSessionStatsPanel';
 
 export default function Home() {
   const [abtUiLanguage, setAbtUiLanguage] = useState<AbtUiLanguage>('high_galactic');
@@ -27,37 +28,7 @@ export default function Home() {
         <div className={styles['b-game-field']}>
           <AbtActiveTaskPanel />
 
-          <div className={styles['b-statistics']}>
-            <div className={styles['b-statistics__caption']}>
-              <span>Result</span>
-            </div>
-            <div className={styles['b-statistics__container']}>
-              <div className={styles['b-statistics__answer']}>
-                <div className={styles['b-answer-indicator']}>
-                  <div className={styles['b-answer-indicator__text']} id="js-answer-text">Idle</div>
-                  <div className={styles['b-answer-indicator__explanation']} id="js-answer-explanation">
-                    <span className={styles['b-answer-indicator__explanation__aub']} id="js-answer-explanation__aub">A</span>
-                    =
-                    <span className={styles['b-answer-indicator__explanation__hg']} id="js-answer-explanation__hg">?</span>
-                  </div>
-                </div>
-              </div>
-              <div className={styles['b-statistics__totals']}>
-                <div className={styles['b-statistics__row']}>
-                  <span className={styles['b-statistics__row-name']}>Attempts </span>
-                  <span className={styles['b-statistics__row-value']} id="js-total-tasks">0</span>
-                </div>
-                <div className={styles['b-statistics__row']}>
-                  <span className={styles['b-statistics__row-name']}>Hits </span>
-                  <span className={styles['b-statistics__row-value']} id="js-total-hits">0</span>
-                </div>
-                <div className={styles['b-statistics__row']}>
-                  <span className={styles['b-statistics__row-name']}>Misses </span>
-                  <span className={styles['b-statistics__row-value']} id="js-total-misses">0</span>
-                </div>
-              </div>
-            </div>
-          </div>
+          <AbtSessionStatsPanel />
 
           <div className={styles['b-status']}>
             <div className={styles['b-status__caption']}>
