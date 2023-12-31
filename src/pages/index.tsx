ee60aba308e90052558b92890624c15f728c7c35
sergei.zhezniakowski <sergei.zhezniakowski@nytimes.com>
2023-12-30 23:09:38 -0500

    nextjs migration: css classes update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import styles from '@/styles/Home.module.css'
 
 export default function Home() {
   return (
@@ -10,90 +11,90 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="b-page">
-        <div className="b-language">
-          <div className="b-language__aurebesh">
-            <a href="#" className="b-language__link" id="js-language-aurebesh">ab</a>
+      <main className={styles['b-page']}>
+        <div className={styles['b-language']}>
+          <div className={styles['b-language__aurebesh']}>
+            <a href="#" className={styles['b-language__link']} id="js-language-aurebesh">ab</a>
           </div>
-          <div className="b-language__high-galactic">
-            <a href="#" className="b-language__link" id="js-language-high-galactic">high galactic</a>
+          <div className={styles['b-language__high-galactic']}>
+            <a href="#" className={styles['b-language__link']} id="js-language-high-galactic">high galactic</a>
           </div>
         </div>
 
-        <div className="b-page-header">Aurebesh Trainer</div>
+        <div className={styles['b-page-header']}>Aurebesh Trainer</div>
 
-        <div className="b-game-field">
-          <div className="b-active-task">
-            <div className="b-active-task__caption">
+        <div className={styles['b-game-field']}>
+          <div className={styles['b-active-task']}>
+            <div className={styles['b-active-task__caption']}>
               <span>Current Task</span>
             </div>
-            <div className="b-active-task__container">
-              <div className="b-active-task__card">
-                    <span id="task-place" className="b-active-task__symbol">
+            <div className={styles['b-active-task__container']}>
+              <div className={styles['b-active-task__card']}>
+                    <span id="task-place" className={styles['b-active-task__symbol']}>
                         A
                     </span>
               </div>
-              <div className="b-active-task__answer">
+              <div className={styles['b-active-task__answer']}>
                 <div>
                   <form id="answer-form">
-                    <label htmlFor="answer" className="b-active-task__answer__label">Answer &gt;</label>
-                    <input type="text" id="answer" autoComplete="off" size="2" className="b-active-task__answer__input" />
-                      <button type="submit" className="b-active-task__answer__button">OK</button>
+                    <label htmlFor="answer" className={styles['b-active-task__answer__label']}>Answer &gt;</label>
+                    <input type="text" id="answer" autoComplete="off" size="2" className={styles['b-active-task__answer__input']} />
+                      <button type="submit" className={styles['b-active-task__answer__button']}>OK</button>
                   </form>
                 </div>
               </div>
             </div>
           </div>
 
-          <div className="b-statistics">
-            <div className="b-statistics__caption">
+          <div className={styles['b-statistics']}>
+            <div className={styles['b-statistics__caption']}>
               <span>Result</span>
             </div>
-            <div className="b-statistics__container">
-              <div className="b-statistics__answer">
-                <div className="b-answer-indicator">
-                  <div className="b-answer-indicator__text" id="js-answer-text">Idle</div>
-                  <div className="b-answer-indicator__explanation" id="js-answer-explanation">
-                    <span className="b-answer-indicator__explanation__aub" id="js-answer-explanation__aub">A</span>
+            <div className={styles['b-statistics__container']}>
+              <div className={styles['b-statistics__answer']}>
+                <div className={styles['b-answer-indicator']}>
+                  <div className={styles['b-answer-indicator__text']} id="js-answer-text">Idle</div>
+                  <div className={styles['b-answer-indicator__explanation']} id="js-answer-explanation">
+                    <span className={styles['b-answer-indicator__explanation__aub']} id="js-answer-explanation__aub">A</span>
                     =
-                    <span className="b-answer-indicator__explanation__hg" id="js-answer-explanation__hg">?</span>
+                    <span className={styles['b-answer-indicator__explanation__hg']} id="js-answer-explanation__hg">?</span>
                   </div>
                 </div>
               </div>
-              <div className="b-statistics__totals">
-                <div className="b-statistics__row">
-                  <span className="b-statistics__row-name">Attempts </span>
-                  <span className="b-statistics__row-value" id="js-total-tasks">0</span>
+              <div className={styles['b-statistics__totals']}>
+                <div className={styles['b-statistics__row']}>
+                  <span className={styles['b-statistics__row-name']}>Attempts </span>
+                  <span className={styles['b-statistics__row-value']} id="js-total-tasks">0</span>
                 </div>
-                <div className="b-statistics__row">
-                  <span className="b-statistics__row-name">Hits </span>
-                  <span className="b-statistics__row-value" id="js-total-hits">0</span>
+                <div className={styles['b-statistics__row']}>
+                  <span className={styles['b-statistics__row-name']}>Hits </span>
+                  <span className={styles['b-statistics__row-value']} id="js-total-hits">0</span>
                 </div>
-                <div className="b-statistics__row">
-                  <span className="b-statistics__row-name">Misses </span>
-                  <span className="b-statistics__row-value" id="js-total-misses">0</span>
+                <div className={styles['b-statistics__row']}>
+                  <span className={styles['b-statistics__row-name']}>Misses </span>
+                  <span className={styles['b-statistics__row-value']} id="js-total-misses">0</span>
                 </div>
               </div>
             </div>
           </div>
 
-          <div className="b-status">
-            <div className="b-status__caption">
+          <div className={styles['b-status']}>
+            <div className={styles['b-status__caption']}>
               <span>Progress</span>
             </div>
-            <div className="b-status__container">
-              <div className="p-status__to-learn">
-                <div className="p-status__to-learn__caption">To Learn</div>
+            <div className={styles['b-status__container']}>
+              <div className={styles['p-status__to-learn']}>
+                <div className={styles['p-status__to-learn__caption']}>To Learn</div>
                 <div id="js-letters-to-learn">
                 </div>
               </div>
-              <div className="p-status__in-learning">
-                <div className="p-status__in-learning__caption">Learning</div>
+              <div className={styles['p-status__in-learning']}>
+                <div className={styles['p-status__in-learning__caption']}>Learning</div>
                 <div id="js-letters-in-learning">
                 </div>
               </div>
-              <div className="p-status__well-learnt">
-                <div className="p-status__well-learnt__caption">Learnt</div>
+              <div className={styles['p-status__well-learnt']}>
+                <div className={styles['p-status__well-learnt__caption']}>Learnt</div>
                 <div id="js-letters-well-learnt">
                 </div>
               </div>
@@ -102,27 +103,27 @@ export default function Home() {
 
         </div>
 
-        <div className="b-font-sample__aurebesh">
+        <div className={styles['b-font-sample__aurebesh']}>
           <span>abcdefghijklmnopqrstuvwxyz1234567890thshookhngcheoae</span>
         </div>
 
-        <div className="b-font-sample__aurebesh_inv">
+        <div className={styles['b-font-sample__aurebesh_inv']}>
           <span>abcdefghijklmnopqrstuvwxyz1234567890thshookhngcheoae</span>
         </div>
 
-        <div className="b-font-sample__aurebesh_tech">
+        <div className={styles['b-font-sample__aurebesh_tech']}>
           <span>abcdefghijklmnopqrstuvwxyz1234567890thshookhngcheoae</span>
         </div>
 
-        <div className="b-font-sample__aurebesh_tech_inv">
+        <div className={styles['b-font-sample__aurebesh_tech_inv']}>
           <span>abcdefghijklmnopqrstuvwxyz1234567890thshookhngcheoae</span>
         </div>
 
-        <div className="b-font-sample__aurebesh_inverted_capitals">
+        <div className={styles['b-font-sample__aurebesh_inverted_capitals']}>
           <span>abcdefghijklmnopqrstuvwxyz1234567890thshookhngcheoae</span>
         </div>
 
-          <span className="b-status__letter">[letter.letter]</span>
+          <span className={styles['b-status__letter']}>[letter.letter]</span>
           {/*<span>{{letter.hits}}</span>*/}
           {/*<span>{{letter.misses}}</span>*/}
           {/*<span>{{letter.hitsAfterLastMiss}}</span>*/}
